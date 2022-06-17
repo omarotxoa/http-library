934f57d0b98c1eafbe7c476d36a615e5e0894b2e
Omar Ochoa <omar@ochoawebdesign.com>
2022-06-16 17:48:14 -0700

    Working POST request

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ const http = new easyHTTP;
 // });
 
 // Get Single Posts
-http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, post) {
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, post) {
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+
+// POST request
+
+// Create Data
+const data = {
+    title: 'Custom Post', 
+    body: 'This is a custom post'
+};
+
+// Create Post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post) {
     if(error) {
         console.log(error);
     } else {
