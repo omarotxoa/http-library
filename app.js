7cee5dd092aa3be143f5b21747a6ca7cd5634b9b
Omar Ochoa <omar@ochoawebdesign.com>
2022-06-16 17:45:02 -0700

    just a single post get request example

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts) {
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+// Get Single Posts
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, post) {
     if(error) {
         console.log(error);
     } else {
-        console.log(posts);
+        console.log(post);
     }
 });
+
