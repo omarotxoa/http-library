d9dbc7ff586e9bd078a29cbe9ff95f337f9b3845
Omar Ochoa <omar@ochoawebdesign.com>
2022-06-16 17:38:58 -0700

    Basic error handling

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(posts) {
-    console.log(posts);
+http.get('https://jsonplaceholder.typicode.com/posts', function(error, posts) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(posts);
+    }
 });
